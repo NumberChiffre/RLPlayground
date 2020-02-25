added prioritized experience replay, not tested yet fixed multi step Deep Expected SARSA using huber loss and gradient clipping fully writes q-loss, q-value, episode length to tensorboard and correctly saves into nested path per experiment

RLPlayground/agents/td.py
@@ -29,14 +29,13 @@ class TDAgent(Agent):
         self.eps = agent_cfg['eps']
         self.gamma = agent_cfg['gamma']
         self.n_step = agent_cfg['n_step']
-        self.alpha = agent_cfg['alpha']
         self.algo = agent_cfg['algo']
         self.update_freq = agent_cfg['update_freq']
 
         # details for the NN model + experience replay
         self.replay_buffer = ReplayBuffer(
             capacity=agent_cfg['replay_capacity'], n_step=self.n_step)
-        self.model = LinearFCBody(
+        self.policy_model = LinearFCBody(
             state_dim=self.env.observation_space.shape[0],
             action_dim=self.env.action_space.n,
             hidden_units=tuple(agent_cfg['nn_hidden_units'])
@@ -48,7 +47,7 @@ class TDAgent(Agent):
         )
         self.lr = agent_cfg['lr']
         self.batch_size = agent_cfg['batch_size']
-        self.optimizer = optim.Adam(self.model.parameters(), self.lr)
+        self.optimizer = optim.Adam(self.policy_model.parameters(), self.lr)
 
         # Huber loss acts like the mean squared error when the error is small,
         # but like the mean absolute error when the error is large
@@ -58,13 +57,13 @@ class TDAgent(Agent):
         self.loss_func = nn.SmoothL1Loss()
         self.writer = writer
 
-    def get_action(self, observation):
+    def get_action(self, observation) -> int:
         """either take greedy action or explore with epsilon rate"""
         if np.random.random() < self.eps:
             return self.env.action_space.sample()
         else:
             state = torch.FloatTensor(observation)
-            q_value = self.model(state)
+            q_value = self.policy_model(state)
             action = q_value.max(1)[1].data[0].item()
             return action
 
@@ -76,9 +75,11 @@ class TDAgent(Agent):
             next_q = self.target_model(batch.s1). \
                 gather(1, batch.a.unsqueeze(1)).squeeze(1)
         elif self.algo == RLAlgorithm.EXPECTED_SARSA.value:
-            next_q = torch.sum(self.target_model(batch.s1), axis=1)
+            next_q = torch.sum(
+                self.target_model(batch.s1) * 1 / self.env.action_space.n,
+                axis=1)
         expected_q = batch.r + self.gamma * (1 - batch.done) * next_q
-        q = self.model(batch.s0).gather(1, batch.a.unsqueeze(
+        q = self.policy_model(batch.s0).gather(1, batch.a.unsqueeze(
             1)).squeeze(1)
 
         # loss = 0.5 * (expected_q.detach() - q).pow(2).mean()
@@ -88,27 +89,30 @@ class TDAgent(Agent):
 
         # gradient clipping to avoid loss divergence based on DeepMind's DQN in
         # 2015, where the author clipped the gradient within [-1, 1]
-        # for param in self.model.parameters():
+        # for param in self.policy_model.parameters():
         #     param.grad.data.clamp_(-1, 1)
-        nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+        nn.utils.clip_grad_norm_(self.policy_model.parameters(), 1.0)
         self.optimizer.step()
 
         # soft update rule
         if self.train_count != 0 and self.update_freq % self.train_count == 0:
-            self.target_model.load_state_dict(self.model.state_dict())
+            self.target_model.load_state_dict(self.policy_model.state_dict())
         return float(loss.detach().cpu().numpy()), float(
             np.mean(q.detach().cpu().numpy()))
 
-    def generate_n_step_q(self):
+    def generate_n_step_q(self) -> Transition:
         """transition tuple: (s0, a, s1, r, done)"""
         transitions = self.replay_buffer.n_step_memory
         reward, next_observation, done = transitions[-1][-3:]
 
-        for _, _, rew, next_obs, do in reversed(list(transitions)[: -1]):
-            reward = self.gamma * reward * (1 - do) + rew
-            next_observation, done = (next_obs, do) if do else (
-                next_observation, done)
-        return reward, next_observation, done
+        for i in range(len(transitions) - 1):
+            reward = self.gamma * reward * (1 - transitions[i].done) + \
+                     transitions[i].r
+            next_observation, done = (transitions[i].s1, transitions[i].done) \
+                if transitions[i].done else (next_observation, done)
+        observation, action = transitions[0][:2]
+        return Transition(s0=observation, a=action, r=reward,
+                          s1=next_observation, done=done)
 
     def interact(self, num_steps: int, episode: int) -> Tuple[float, int]:
         """use agent to interact with environment by making actions based on
@@ -122,16 +126,16 @@ class TDAgent(Agent):
             next_observation = np.expand_dims(next_observation, 0)
             cr += reward
 
-            # if we use n step
-            if self.n_step > 0:
-                if t % self.n_step == 0 and t != 0:
-                    reward, next_observation, done = self.generate_n_step_q()
-
             # store into experience replay buffer and sample batch of
             # transitions to estimate the q-values and train on losses
-            transition = Transition(s0=observation, a=action,
-                                    s1=next_observation, r=reward, done=done)
+            transition = Transition(s0=observation, a=action, r=reward,
+                                    s1=next_observation, done=done)
+            if self.n_step > 0:
+                if t % self.n_step == 0 and t != 0:
+                    transition = self.generate_n_step_q()
             self.replay_buffer.push(transition)
+
+            # train policy network and update target network
             if len(self.replay_buffer.memory) > self.batch_size:
                 loss, q = self.train()
                 self.writer.add_scalar(tag='Training/Q-loss', scalar_value=loss,
@@ -142,9 +146,7 @@ class TDAgent(Agent):
                 print(
                     f'episode {episode} | step {t} | loss {loss} | q-value {q}')
             observation = next_observation
-
-            # last step reached in episode
-            # update the target network
+            # self.env.render()
             if done:
                 num_steps = t + 1
                 print(f'episode {episode} finished at {num_steps} steps')

RLPlayground/experiments/td_experiments.py
@@ -20,8 +20,7 @@ class TDExperiment(Experiment):
         super().__init__(logger=logger, *args, **kwargs)
         self.replay_buffer_capacities = self.experiment_cfg[
             'replay_buffer_capacities']
-        self.lrs = np.array([float(i / 100) for i in range(0, 101, 10)])
-        # self.lrs = np.array([0.8, 0.9])
+        self.lrs = np.array([float(i / 1000) for i in range(1, 201, 10)])
         self.experiment_cfg['lrs'] = self.lrs
 
     def generate_metrics(self, env_name: str, algo: str, capacity: int,
@@ -51,6 +50,9 @@ class TDExperiment(Experiment):
             np.vstack([results[i][1] for i in range(len(results))]),
             axis=0)
         output[env_name][algo][capacity]['min_timesteps'] = np.min(
+            np.vstack([results[i][1] for i in range(len(results))]),
+            axis=0)
+        output[env_name][algo][capacity]['max_timesteps'] = np.max(
             np.vstack([results[i][1] for i in range(len(results))]),
             axis=0)
         return output
@@ -85,9 +87,8 @@ class TDExperiment(Experiment):
     def _inner_run(agent_cfg: dict, experiment_cfg: dict, env_name: str,
                    seed: int = 1, algo: str = 'sarsa') -> Tuple[
         np.array, np.array]:
+        # seed and result initialization
         np.random.seed(seed)
-
-        # result initialization
         cum_reward = np.zeros((len(experiment_cfg['lrs']),
                                experiment_cfg['runs'],
                                experiment_cfg['episodes']))
@@ -96,24 +97,28 @@ class TDExperiment(Experiment):
                                  experiment_cfg['episodes'])) * experiment_cfg[
                             'steps']
 
+        # O(lrs * runs * episodes * max(test_rng * steps, steps))
         for i_lr in range(len(experiment_cfg['lrs'])):
-            # create environment and agent, set the learning rate
+            # create environment and agent
+            # set the learning rate
+            # set the tensorboard path..
             env = gym.make(env_name)
-            agent_cfg[env_name][algo]['lr'] = experiment_cfg['lrs'][i_lr]
+            agent_config = agent_cfg[env_name][algo]
+            agent_config['lr'] = experiment_cfg['lrs'][i_lr]
             writer = SummaryWriter(
                 log_dir=f"{experiment_cfg['tensorboard_path']}/{env_name}/"
-                f"{algo}/replay_buffer_capacity/"
-                f"{agent_cfg[env_name][algo]['replay_capacity']}/lr/"
-                f"{agent_cfg[env_name][algo]['lr']}/seed/{seed}")
-            agent = TDAgent(env=env, writer=writer,
-                            agent_cfg=agent_cfg[env_name][algo])
+                f"{algo}/{experiment_cfg['date']}/n_step/{agent_config['n_step']}/"
+                f"replay_buffer_capacity/{agent_config['replay_capacity']}/lr/"
+                f"{agent_config['lr']}/seed/{seed}")
+            agent = TDAgent(env=env, writer=writer, agent_cfg=agent_config)
 
-            # O(runs * episodes * max(test_rng * steps, steps))
+            # go through runs, in order to further average, and episodes
             for r in range(experiment_cfg['runs']):
                 for i_episode in range(experiment_cfg['episodes']):
-                    # interact with environment to get reward
                     cr, t = agent.interact(num_steps=experiment_cfg['steps'],
                                            episode=i_episode)
+                    writer.add_scalar(tag='Episode-Duration', scalar_value=t,
+                                           global_step=i_episode)
                     time_to_solve[i_lr, r, i_episode] = t
                     cum_reward[i_lr, r, i_episode] = cr
                     print(f"lr {experiment_cfg['lrs'][i_lr]} | "

RLPlayground/models/replay.py
@@ -1,7 +1,8 @@
 import random
 from collections import deque
-from typing import List
+from typing import List, Tuple
 import torch
+import numpy as np
 
 from RLPlayground.utils.data_structures import Transition
 
@@ -46,17 +47,51 @@ class ReplayBuffer:
 
 
 class PrioritizedReplayBuffer(ReplayBuffer):
-    def __init__(self, capacity: int, alpha: float):
+    def __init__(self, capacity: int, n_step: int, alpha: float, beta: float,
+                 beta_inc: float):
         """
 
         :param capacity: maximum number of transition tuple stored in replay
+        :param n_step: n step used for replay
         :param alpha: 0 for no prioritization, 1 for full prioritization
+        :param beta:
+        :param beta_inc:
         """
-        super(PrioritizedReplayBuffer, self).__init__(capacity=capacity)
+        # try with alpha=0.6, beta=0.4, beta_inc=100~network update frequency
+        super().__init__(capacity=capacity, n_step=n_step)
         self.alpha = alpha
+        self.beta = beta
+        self.beta_inc = beta_inc
+        self.priorities = np.zeros([capacity], dtype=np.float32)
 
-    def push(self, *args, **kwargs):
-        super().push(*args)
+    def sample(self, batch_size: int) -> Tuple[List[Transition], np.array]:
+        if len(self.memory) < self.capacity:
+            probs = self.priorities[:len(self.memory)]
+        else:
+            probs = self.priorities
+        probs = (probs ** self.alpha) / np.sum(probs ** self.alpha)
+        indices = np.random.choice(len(self.memory), batch_size, p=probs)
+        if self.beta < 1:
+            self.beta += self.beta_inc
 
-    def _sample_proportional(self, batch_size: int):
-        pass
+        # samples a batch from memory and concatenates the dimensions of
+        # observations and convert to torch
+        # cannot use super().sample as we need the indices
+        batch = [self.memory[idx] for idx in indices]
+        observation, action, reward, next_observation, done = zip(*batch)
+        observation = torch.cat(tuple(torch.FloatTensor(observation)), dim=0)
+        action = torch.LongTensor(action)
+        reward = torch.FloatTensor(reward)
+        next_observation = torch.cat(tuple(torch.FloatTensor(next_observation)),
+                                     dim=0)
+        done = torch.FloatTensor(done)
+
+        # update priorities
+        for idx, priority in zip(indices, self.priorities):
+            self.priorities[idx] = priority
+
+        # need weights to compute MSE
+        weights = (len(self.memory) * probs[indices]) ** -self.beta
+        weights = np.array(weights / np.max(weights), dtype=np.float32)
+        return Transition(s0=observation, a=action, r=reward,
+                          s1=next_observation, done=done), weights